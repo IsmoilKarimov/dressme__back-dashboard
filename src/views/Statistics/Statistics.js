d3152c65e64c0ddd77f0ef47a5d93dc33b2c9e5f
main <ismoilergashovich@gmail.com>
2023-05-30 07:46:38 +0500

    add

--- src/views/Statistics/Statistics.js
+++ src/views/Statistics/Statistics.js
@@ -4,6 +4,7 @@ export default function Statistics() {
   return (
     <div>
       <h1 className='text-3xl'>Statistics</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci ullam sit possimus quam quos laudantium ut dolor dolore. Quas alias eveniet cumque nam voluptatem facere quos, tempora quae possimus officiis laboriosam molestias earum ipsum doloremque atque architecto pariatur vitae libero adipisci reiciendis distinctio consequatur quidem temporibus! Soluta adipisci quos laboriosam?</p>
     </div>
   )
 }

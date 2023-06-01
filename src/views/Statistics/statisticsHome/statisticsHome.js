d9ac4fbd2324369b2a9ab0ca92a3962ca5ae1be5
main <ismoilergashovich@gmail.com>
2023-06-01 23:39:24 +0500

    add

--- src/views/Statistics/statisticsHome/statisticsHome.js
+++ src/views/Statistics/statisticsHome/statisticsHome.js
@@ -4,6 +4,7 @@ export default function StatisticsHome() {
   return (
     <div className='p-10'>
       <h1 className='text-3xl'>Statistics</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi fugit dolorem dicta! Similique incidunt harum numquam corporis explicabo dolore culpa ad sed sint exercitationem aliquam facere et iste delectus, libero vitae molestiae cum fuga, consectetur saepe ipsam aliquid ea. Illo consequuntur in eveniet delectus itaque quam quia ea eaque. Eaque corporis vitae sequi dignissimos libero soluta sapiente illum dolores atque ducimus rem odit, fuga recusandae ullam! Sint quisquam obcaecati, suscipit laborum, ratione natus iure deserunt repudiandae totam molestiae repellat tempore harum dolor modi amet. Minus magnam dignissimos fugiat assumenda amet ea, perferendis quibusdam aperiam dolorum earum ipsa sit. Tempore, aut.</p>
     </div>
   )
 }
